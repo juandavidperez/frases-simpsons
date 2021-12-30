d8119b79f7324ddbd3faf4e9eef73f97cd0a7a72
unknown <juandavidperezvergara@gmail.com>
2021-12-30 16:55:04 -0500

    agrega el servidor al handler de netlify

--- functions/frases/frases.js
+++ functions/frases/frases.js
@@ -1,17 +1,41 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+const quotes = require('../quotes.json');
+
+// funciones para filtrar los datos
+const findQuote = (valor) => {
+    return quotes.find(frase => frase.quote.toLowerCase().includes(valor.toLowerCase()));
 }
 
-module.exports = { handler }
+const findCharacterQuotes = (name) => quotes.filter(frase => {
+    const nameToLowerCase = name.toLowerCase();
+    const characterNameToLowerCase = frase.character.toLowerCase();
+    return characterNameToLowerCase.includes(nameToLowerCase);
+});
+
+app.use(bodyParser);
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+  res.send('Hello World!');
+})
+
+app.get('/character', (req, res) => {
+    const characterQuotes = findCharacterQuotes(req.query.name || '');
+  res.json(characterQuotes);
+})
+
+app.get('/quote', (req, res) => {
+    const quote = findQuote(req.query.quote || '');
+  res.json(quote);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports.handler = serverless(app);
