0c96a3d24d653158dfd798676b48719fbef9d8f6
unknown <juandavidperezvergara@gmail.com>
2021-12-30 17:03:20 -0500

    mueve las frases junto a la funcion para evitar error del build

--- functions/frases/frases.js
+++ functions/frases/frases.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
-const quotes = require('../quotes.json');
+const quotes = require('./functions/quotes.json');
 
 // funciones para filtrar los datos
 const findQuote = (valor) => {
